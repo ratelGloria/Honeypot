059f9a585291f4061a9bdfe6e8301f0aca8fc4aa
ratelGloria <1007227291@qq.com>
2018-11-12 23:48:37 +0800

    页面

--- js/mypage.js
+++ js/mypage.js
@@ -3,13 +3,14 @@ var username = document.getElementById("userName");
 var password = document.getElementById("passWord");
 var userpwd = document.getElementById("userApwd");
 var submit1 = document.getElementById("submit");
-/*userpwd.style.display = "none";*/
+userpwd.style.position = "absolute";
+userpwd.style.display = "none";
 var count = 0;
 essay.onclick = function(){
     console.log("push");
-    userpwd.style.dispaly = "inline";
+    userpwd.style.display = "block";
     count++;
-    submit1.onclick = privateLogIn();
+    /*submit1.onclick = privateLogIn();*/
 }
 function privateLogIn(){
     if(count<=3&&username === "14102026" &&password === "stykiss1229"){
