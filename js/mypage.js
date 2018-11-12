29148786c1fa03e030e56bdf6d99063de2b2ac71
ratelGloria <1007227291@qq.com>
2018-11-12 23:51:50 +0800

    页面

--- js/mypage.js
+++ js/mypage.js
@@ -9,8 +9,7 @@ var count = 0;
 essay.onclick = function(){
     console.log("push");
     userpwd.style.display = "block";
-   /* count++;*/
-    /*submit1.onclick = privateLogIn();*/
+
 }
 function privateLogIn(){
     if(count<=3&&username === "14102026" &&password === "stykiss1229"){
