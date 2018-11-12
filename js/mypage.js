7fb5dfc97822281cc09c8705f644076ca6af0453
ratelGloria <1007227291@qq.com>
2018-11-12 23:03:16 +0800

    页面

---
+++ js/mypage.js
@@ -0,0 +1,17 @@
+var essay = document.getElementById("privatetoother");
+var username = document.getElementById("userName");
+var password = document.getElementById("passWord");
+var userpwd = document.getElementById("userApwd");
+var submit1 = document.getElementById("submit");
+userpwd.style.display="none";
+var count = 0;
+essay.onclick = function(){
+    userpwd.style.dispaly = "inline";
+    count++;
+    submit1.onclick = privateLogIn();
+}
+function privateLogIn(){
+    if(count<=3&&username === "14102026" &&password === "stykiss1229"){
+        self.location = "privateMessage.html";
+    }
+}
