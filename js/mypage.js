4d062e5df7a9da15e08fe74a6295d2a734574ad8
ratelGloria <1007227291@qq.com>
2018-11-13 10:15:14 +0800

    页面

--- js/mypage.js
+++ js/mypage.js
@@ -1,18 +1,31 @@
-var essay = document.getElementById("privatetoother");
-var username = document.getElementById("userName");
-var password = document.getElementById("passWord");
-var userpwd = document.getElementById("userApwd");
-var submit1 = document.getElementById("submit");
-userpwd.style.position = "absolute";
-userpwd.style.display = "none";
-var count = 0;
-essay.onclick = function(){
-    console.log("push");
-    userpwd.style.display = "block";
+window.onload = function() {
+    var essay = document.getElementById("privatetoother");
+    var username = document.getElementById("userName");
+    var password = document.getElementById("passWord");
+    var userpwd = document.getElementById("userApwd");
+    var submit_log_in = document.getElementById("submit_log_in");
+    var submit_cancel = document.getElementById("submit_cancel");
+    userpwd.style.position = "absolute";
+    userpwd.style.display = "none";
+    var count = 0;
+    essay.onclick = function () {
+        console.log("push");
+        userpwd.style.display = "block";
+
+
+    }
+    submit_log_in.onclick = function () {
+        console.log("我进入了");
+        if (username === "14102026" && password === "stykiss1229") {
+            console.log("我进入了");
+            window.location.href = 'https://ratelgloria.github.io/Honeypot.github.io/pages/privateMessage.html';
+            self.location = "privateMessage.html";
+        }
+    }
+
+    submit_cancel.onclick = function () {
+        userpwd.style.display = "none";
+    }
 
 }
-function privateLogIn(){
-    if(count<=3&&username === "14102026" &&password === "stykiss1229"){
-        self.location = "privateMessage.html";
-    }
-}
+
